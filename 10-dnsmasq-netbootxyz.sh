4649581df1e89370abdec3f032cf14eb921448ff
Andy Attebery <me@andyattebery.com>
2024-01-05 12:53:11 -0600

    initial commit

---
+++ 10-dnsmasq-netbootxyz.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+NETBOOTXYZ_SERVER_IP="192.168.1.232"
+
+cat << EOF > /run/dnsmasq.conf.d/dhcp.netbootxyz.conf
+# Configures netboot.xyz PXE boot
+
+# -U, --dhcp-vendorclass=set:<tag>,[enterprise:<IANA-enterprise number>,]<vendor-class>
+# Map from a vendor-class string to a tag. Most DHCP clients provide a "vendor class" which represents, in some sense, the type of host. This option maps vendor classes to tags, so that DHCP options may be selectively delivered to different classes of hosts. For example --dhcp-vendorclass=set:printers,Hewlett-Packard JetDirect will allow options to be set only for HP printers like so: --dhcp-option=tag:printers,3,192.168.4.4 The vendor-class string is substring matched against the vendor-class supplied by the client, to allow fuzzy matching. The set: prefix is optional but allowed for consistency.
+# Note that in IPv6 only, vendorclasses are namespaced with an IANA-allocated enterprise number. This is given with enterprise: keyword and specifies that only vendorclasses matching the specified number should be searched.
+# Architecture numbers from: https://www.iana.org/assignments/dhcpv6-parameters/dhcpv6-parameters.xhtml#processor-architecture
+
+# x86 BIOS
+dhcp-vendorclass=set:bios,PXEClient:Arch:00000
+# x86 UEFI
+dhcp-vendorclass=set:uefi_x86,PXEClient:Arch:00006
+# x64 UEFI
+dhcp-vendorclass=set:uefi_x64,PXEClient:Arch:00007
+# EBC
+dhcp-vendorclass=set:uefi_x64,PXEClient:Arch:00009
+# ARM 32-bit UEFI
+dhcp-vendorclass=set:uefi_arm32,PXEClient:Arch:00010
+# ARM 64-bit UEFI
+dhcp-vendorclass=set:uefi_arm64,PXEClient:Arch:00011
+
+# -M, --dhcp-boot=[tag:<tag>,]<filename>,[<servername>[,<server address>|<tftp_servername>]]
+# (IPv4 only) Set BOOTP options to be returned by the DHCP server. Server name and address are optional: if not provided, the name is left empty, and the address set to the address of the machine running dnsmasq. If dnsmasq is providing a TFTP service (see --enable-tftp ) then only the filename is required here to enable network booting. If the optional tag(s) are given, they must match for this configuration to be sent. Instead of an IP address, the TFTP server address can be given as a domain name which is looked up in /etc/hosts. This name can be associated in /etc/hosts with multiple IP addresses, which are used round-robin. This facility can be used to load balance the tftp load among a set of servers.
+# dhcp-boot=tag:<tag>,<boot_image_file>,<servername>,<tftp_server_ip>
+dhcp-boot=tag:bios,netboot.xyz.kpxe,netboot.xyz,$NETBOOTXYZ_SERVER_IP
+dhcp-boot=tag:uefi_x86,netboot.xyz.efi,netboot.xyz,$NETBOOTXYZ_SERVER_IP
+dhcp-boot=tag:uefi_x64,netboot.xyz.efi,netboot.xyz,$NETBOOTXYZ_SERVER_IP
+dhcp-boot=tag:uefi_arm32,netboot.xyz-arm64.efi,netboot.xyz,$NETBOOTXYZ_SERVER_IP
+dhcp-boot=tag:uefi_arm64,netboot.xyz-arm64.efi,netboot.xyz,$NETBOOTXYZ_SERVER_IP
+EOF
+
+kill `cat /run/dnsmasq.pid`
---
+++ README.md
@@ -0,0 +1,32 @@
+# Unifi On Boot Scripts
+
+## Unifi On Boot Script
+
+This enables persistent scripts on Unifi gateways that run on boot.
+
+### Installation
+
+1. [Enable ssh for UnifiOS](https://help.ui.com/hc/en-us/articles/204909374-UniFi-Connect-with-SSH-Advanced-)
+    - For the UDMP, the username is `root`
+2. [Install the Unifi On Boot Script](https://github.com/unifi-utilities/unifios-utilities/blob/main/on-boot-script/README.md)
+    - The "magic" install script has different installation methods for different Unifi devices.
+    - For the UDMP, it installs a systemd service (which is a different method than the _Manual Install Steps_ in the README).
+
+## dnsmasq netboot.xyz Script
+
+This configures the Unifi DHCP server for PXE booting from a local [netboot.xyz](netboot.xyz) installation.
+
+The built-in Unifi DCHP server is dnsmasq and is configured to read configuration files from `/run/dnsmasq.conf.d/`. However, this is mounted on tmpfs and is therefore wiped on reboot. So the `10-dnsmasq-netbootxyz.sh` script creates a configuration file — `dhcp.netbootxyz.conf` — on boot.
+
+ The `dhcp.netbootxyz.conf` configuration uses the `dhcp-boot` parameter to configure [DHCP option 66 (TFPT server) and DHCP option 67 (boot file)](https://www.rfc-editor.org/rfc/rfc2132.html). It also handles different architectures (i.e. BIOS, UEFI x64, UEFI arm64, etc) which need specific boot files using the `dhcp-vendorclass` parameter to set different tags to be matched by specific `dhcp-boot` parameters.
+
+### Installation
+
+1. Set the `NETBOOTXYZ_SERVER_IP` variable at the top of the `10-dnsmasq-netbootxyz.sh` file to your netboot.xyz server IP.
+2. Copy the `10-dnsmasq-netbootxyz.sh` file to your Unifi gateway.
+    - `scp 10-dnsmasq-netbootxyz.sh root@<unifi-ip>:/data/on_boot.d`
+3. Restart the udm-boot.service to run the script.
+    - `systemctl restart udm-boot.service`
+
+ ## References
+ - https://community.ui.com/questions/PXE-Network-boot-UDM-SE-Serving-files-conditionally-based-on-architecture/1843fcf6-87d5-4305-bc1d-4e55619ebb10
